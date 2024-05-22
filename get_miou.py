e7e7618632c3926394e8fb878f066f4ee4fcaf29
wangqiuji1 <58511832+wangqiuji1@users.noreply.github.com>
2024-05-22 21:10:06 +0800

    Add files via upload

---
+++ get_miou.py
@@ -0,0 +1,48 @@
+import os
+
+from PIL import Image
+from tqdm import tqdm
+
+from deeplab import DeeplabV3
+from utils.utils_metrics import compute_mIoU, show_results
+
+
+if __name__ == "__main__":
+    #---------------------------------------------------------------------------#
+    #   Calculate and evaluate indicators
+    #---------------------------------------------------------------------------#
+    miou_mode       = 0
+
+    num_classes     = 6
+
+    name_classes    = ["_background_", "corrosionLevel1", "corrosionLevel2", "corrosionLevel3", "corrosionLevel4", "corrosionLevel5"]
+    # name_classes    = ["_background_","cat","dog"]
+
+    VOCdevkit_path  = 'VOCdevkit'
+
+    image_ids       = open(os.path.join(VOCdevkit_path, "VOC2007/ImageSets/Segmentation/val.txt"),'r').read().splitlines() 
+    gt_dir          = os.path.join(VOCdevkit_path, "VOC2007/SegmentationClass/")
+    miou_out_path   = "miou_out"
+    pred_dir        = os.path.join(miou_out_path, 'detection-results')
+
+    if miou_mode == 0 or miou_mode == 1:
+        if not os.path.exists(pred_dir):
+            os.makedirs(pred_dir)
+            
+        print("Load model.")
+        deeplab = DeeplabV3()
+        print("Load model done.")
+
+        print("Get predict result.")
+        for image_id in tqdm(image_ids):
+            image_path  = os.path.join(VOCdevkit_path, "VOC2007/JPEGImages/"+image_id+".jpg")
+            image       = Image.open(image_path)
+            image       = deeplab.get_miou_png(image)
+            image.save(os.path.join(pred_dir, image_id + ".png"))
+        print("Get predict result done.")
+
+    if miou_mode == 0 or miou_mode == 2:
+        print("Get miou.")
+        hist, IoUs, PA_Recall, Precision = compute_mIoU(gt_dir, pred_dir, image_ids, num_classes, name_classes)  # 执行计算mIoU的函数
+        print("Get miou done.")
+        show_results(miou_out_path, hist, IoUs, PA_Recall, Precision, name_classes)
---
+++ voc_annotation.py
@@ -0,0 +1,89 @@
+import os
+import random
+
+import numpy as np
+from PIL import Image
+from tqdm import tqdm
+
+
+trainval_percent    = 1
+train_percent       = 0.9
+#-------------------------------------------------------#
+#   dataset path
+#-------------------------------------------------------#
+VOCdevkit_path      = 'VOCdevkit'
+
+if __name__ == "__main__":
+    random.seed(0)
+    print("Generate txt in ImageSets.")
+    segfilepath     = os.path.join(VOCdevkit_path, 'VOC2007/SegmentationClass')
+    saveBasePath    = os.path.join(VOCdevkit_path, 'VOC2007/ImageSets/Segmentation')
+    
+    temp_seg = os.listdir(segfilepath)
+    total_seg = []
+    for seg in temp_seg:
+        if seg.endswith(".png"):
+            total_seg.append(seg)
+
+    num     = len(total_seg)  
+    list    = range(num)  
+    tv      = int(num*trainval_percent)  
+    tr      = int(tv*train_percent)  
+    trainval= random.sample(list,tv)  
+    train   = random.sample(trainval,tr)  
+    
+    print("train and val size",tv)
+    print("traub suze",tr)
+    ftrainval   = open(os.path.join(saveBasePath,'trainval.txt'), 'w')  
+    ftest       = open(os.path.join(saveBasePath,'test.txt'), 'w')  
+    ftrain      = open(os.path.join(saveBasePath,'train.txt'), 'w')  
+    fval        = open(os.path.join(saveBasePath,'val.txt'), 'w')  
+    
+    for i in list:  
+        name = total_seg[i][:-4]+'\n'  
+        if i in trainval:  
+            ftrainval.write(name)  
+            if i in train:  
+                ftrain.write(name)  
+            else:  
+                fval.write(name)  
+        else:  
+            ftest.write(name)  
+    
+    ftrainval.close()  
+    ftrain.close()  
+    fval.close()  
+    ftest.close()
+    print("Generate txt in ImageSets done.")
+
+    print("Check datasets format, this may take a while.")
+    classes_nums        = np.zeros([256], np.int)
+    for i in tqdm(list):
+        name            = total_seg[i]
+        png_file_name   = os.path.join(segfilepath, name)
+        if not os.path.exists(png_file_name):
+            raise ValueError("未检测到标签图片%s，请查看具体路径下文件是否存在以及后缀是否为png。"%(png_file_name))
+        
+        png             = np.array(Image.open(png_file_name), np.uint8)
+        if len(np.shape(png)) > 2:
+
+            print("The label image needs to be a grayscale map or an eight-bit color map, and the value of each pixel of the label is the kind of pixel it belongs to."%(name, str(np.shape(png))))
+
+        classes_nums += np.bincount(np.reshape(png, [-1]), minlength=256)
+            
+    print("Value and number of printed pixels.")
+    print('-' * 37)
+    print("| %15s | %15s |"%("Key", "Value"))
+    print('-' * 37)
+    for i in range(256):
+        if classes_nums[i] > 0:
+            print("| %15s | %15s |"%(str(i), str(classes_nums[i])))
+            print('-' * 37)
+    
+    if classes_nums[255] > 0 and classes_nums[0] > 0 and np.sum(classes_nums[1:255]) == 0:
+        print("The value of the pixels in the label detected only include 0 and 255, and the data format is wrong.")
+        print("The dichotomization problem requires modifying the label to background with a pixel value of 0 and a target pixel value of 1.")
+    elif classes_nums[0] > 0 and np.sum(classes_nums[1:]) == 0:
+        print("If the tag contains only background pixels, the data format is wrong, please carefully check the data set format.")
+
+    print("The picture in the JPEGImages shall be of. The image in the jpg file, SegmentationClass shall be. png document.")
